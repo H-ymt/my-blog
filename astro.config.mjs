a120e6583c1238b559af1278dfc7ad28d6c85fb7
H-ymt <y.handai1272@gmail.com>
2023-12-14 22:03:16 +0900

    fix: color pallet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,30 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import addClasses from "rehype-add-classes";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  site: "https://example.com",
+  integrations: [sitemap(), tailwind(), react()],
+  markdown: {
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: "text-4xl font-bold",
+          h2: "text-2xl font-bold pb-4",
+          h3: "text-xl font-bold",
+          h4: "text-lg font-bold",
+          h5: "font-bold",
+          h6: "font-bold",
+          img: "border border-slate-300 dark:border-zinc-700 rounded-xl mb-6",
+          p: "mb-6",
+          a: "underline underline-offset-2 hover:text-orange-500 decoration-orange-500",
+          ul: "list-disc",
+        },
+      ],
+    ],
+  },
 });
